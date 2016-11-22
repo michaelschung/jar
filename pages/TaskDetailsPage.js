3d7b5d0f7795c0d80d0218fb0d1f96151d2f446c
David M <mrlsdvd@gmail.com>
2016-11-21 18:24:34 -0800

    Worked on top portion of details page and fixed buttons at the bottom.

--- pages/TaskDetailsPage.js
+++ pages/TaskDetailsPage.js
@@ -18,6 +18,7 @@ var moment = require('moment');
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
     flex: 1,
     paddingTop: 74,
     margin: 12,
@@ -25,27 +26,86 @@ const styles = StyleSheet.create({
   taskName: {
   	fontSize: 24,
   },
-  due: {
-  	fontSize: 18,
-  },
   detailsContainer: {
-  	flex: 1,
+    marginTop: 20,
+    flexDirection: 'row',
+  	flex: 2,
+  },
+  textDetailsContainer: {
+    marginLeft: 20,
+    flexDirection: 'column',
+    flex: 1
+  },
+  taskNameTextContainer: {
+    marginTop: 15,
+    alignSelf: 'stretch',
+    marginBottom: 20,
+  },
+  taskNameText: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+  label: {
+    textAlign: 'left',
+    fontSize: 20,
+  },
+  dueAndTimeContainer: {
+    flexDirection: 'column',
+  },
+  dueInTextContainer: {
+    justifyContent: 'space-between',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    marginBottom: 3,
+  },
+  dueInText: {
+    color: '#319bce',
+    textAlign: 'right',
+    fontSize: 20,
+  },
+  dueInTextUrgent: {
+    color: 'red',
+    textAlign: 'right',
+    fontSize: 20,
+  },
+  timeToCompleteTextContainer: {
+    justifyContent: 'space-between',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    marginTop: 3,
+  },
+  timeToCompleteText: {
+    textAlign: 'right',
+    fontSize: 20,
+  },
+  taskOwnerImageContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  ownerImage: {
+    marginRight: 20,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
   },
   buttonsContainer: {
   	flex: 1,
   	alignItems: 'center',
   },
   button: {
+    justifyContent: 'center',
     backgroundColor: '#319bce',
     marginBottom: 7,
     borderRadius: 15,
     minHeight: 75,
     minWidth: 250,
+    alignItems: 'center',
   },
   buttonText: {
+    fontWeight: '500',
   	color: 'white',
   	alignSelf: 'center',
-  	fontSize: 18,
+  	fontSize: 24,
   }
 });
 
@@ -58,20 +118,48 @@ class TaskDetailsPage extends Component {
 	render() {
 		console.log('rendering details page');
 		var numDays = moment(this.props.due).fromNow();
+    /* Get number of days between now and due date */
+    var dueDate = new Date(this.props.due)
+    var now = new Date()
+    var timeDiff = Math.abs(now.getTime() - dueDate.getTime())
+    var daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    console.log(daysLeft)
+
 		return (
 			<View style={styles.container}>
+        {/* Details about task */}
 				<View style={styles.detailsContainer}>
-					<Text style={styles.taskName}>{this.props.name}</Text>
-					<Text style={styles.due}>{numDays}</Text>
+          <View style={styles.textDetailsContainer}>
+            <View style={styles.taskNameTextContainer}>
+              <Text style={styles.taskNameText}>{this.props.name}</Text>
+            </View>
+            <View style={styles.dueAndTimeContainer}>
+              <View style={styles.dueInTextContainer}>
+                <Text style={styles.label}>Due:</Text>
+                {/* Use urgent style if task is due within 24 hours (1 day) */}
+                <Text style={daysLeft > 1 ? styles.dueInText : styles.dueInTextUrgent}>{numDays}</Text>
+              </View>
+              <View style={styles.timeToCompleteTextContainer}>
+                <Text style={styles.label}>Will Take:</Text>
+                <Text style={styles.timeToCompleteText}>5 min</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.taskOwnerImageContainer}>
+            <Image source={{ uri: 'https://facebook.github.io/react/img/logo_og.png' }} style={styles.ownerImage} />
+          </View>
 				</View>
+
+        {/* Action buttons */}
 				<View style={styles.buttonsContainer}>
-					<TouchableOpacity style={styles.button}>
-						<Text style={styles.buttonText}>Complete Task</Text>
-					</TouchableOpacity>
 					<TouchableOpacity style={styles.button}>
 						<Text style={styles.buttonText}>Transfer Task</Text>
 					</TouchableOpacity>
+					<TouchableOpacity style={styles.button}>
+						<Text style={styles.buttonText}>Complete Task</Text>
+					</TouchableOpacity>
 				</View>
+
 			</View>
 		);
 	}
