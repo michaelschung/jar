7214a96d33420d84dfd1eaa035934d46e846c1d9
Michael Chung <mchung96@stanford.edu>
2016-11-25 02:05:30 -0800

    fake commit #2

---
+++ pages/TasksPage.js
@@ -0,0 +1,232 @@
+'use strict';
+
+import React, { Component } from 'react'
+import TaskRowItem from '../components/TaskRowItem'
+import CreatePage from '../pages/CreatePage'
+import {
+	Alert,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	ActivityIndicator,
+	Image,
+	ListView,
+	TouchableOpacity,
+	SegmentedControlIOS,
+} from 'react-native';
+
+import TaskDetailsPage from './TaskDetailsPage.js'
+
+var moment = require('moment');
+
+var today = new Date();
+var taskList = [
+	{
+		name:'Take out trash', 
+		owner: {name: 'Michael', picURL: 'http://web.stanford.edu/class/cs147/projects/Home/Jar/images/Michael.jpg'},
+		isMyTask: true, 
+		completed:false, 
+		due:new Date().setDate(today.getDate() + 1), 
+		timeToComplete: '5 min'
+	},
+	{
+		name:'Call the landlord', 
+		owner: {name: 'Evan', picURL: 'http://web.stanford.edu/class/cs147/projects/Home/Jar/images/Evan.jpg'}, 
+		completed:false, 
+		due:new Date().setDate(today.getDate() + 3), 
+		timeToComplete: '15 min'
+	},
+	{
+		name:'Clean room',
+		owner: {name: 'David', picURL: 'http://web.stanford.edu/class/cs147/projects/Home/Jar/images/David.JPG'}, 
+		completed:false, 
+		due:new Date().setDate(today.getDate() + 4), 
+		timeToComplete: '30 min' 
+	}, 
+];
+/* 
+	Task object
+		name: name of the task
+		isMyTask: boolean True if task assigned to current user
+		owner: {name: username of owner, picURL: user's icon URL (from website)}
+		completed: boolean True if task has been completed
+		due: JS Date object representing the day the task is due
+		timeToComplete: string representing expected time to complete task
+*/
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12,
+  },
+  taskName: {
+    marginLeft: 12,
+    fontSize: 16,
+  },
+  photo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  checkBox: {
+  	height: 40,
+  	width: 40,
+  	borderRadius: 20,
+  	backgroundColor: '#319bce',
+  },
+  checkBoxIcon: {
+  	height: 40,
+  	width: 40,
+  },
+  due: {
+    textAlign: 'right',
+    flex: 1,
+  },
+  separator: {
+  	flex: 1,
+    height: .5,
+    backgroundColor: '#8E8E8E',
+    padding: 0
+  },
+  segmentSeparator: {
+  	flex: 1,
+    maxHeight: .5,
+    backgroundColor: '#8E8E8E',
+    padding: 0
+  },
+  addButton: {
+    backgroundColor: '#319bce',
+    marginBottom: 20,
+    marginRight: 20,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    borderRadius: 35,
+    minHeight: 50,
+    minWidth: 50,
+    width: 70,
+    height: 70
+  },
+  buttonText: {
+  	color: 'white',
+  	alignSelf: 'center',
+  	marginBottom: 5,
+  	fontSize: 50
+  }
+});
+
+class TasksPage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			dataSource: this.ds.cloneWithRows(taskList),
+			taskView: 'All Tasks',
+		};
+	}
+
+	onCheckboxPressed = (taskItem) => {
+		taskItem.completed = !taskItem.completed;
+		if (this.state.taskView === "All Tasks") {
+			this.setState({
+				dataSource: this.ds.cloneWithRows(taskList)
+			});
+		} else {
+			this.setState({
+				dataSource: this.ds.cloneWithRows(taskList.filter(this.checkTaskIsMine))
+			})
+		}
+
+	}
+
+	onTaskPressed = (taskItem) => {
+		this.props.navigator.push({
+			title: 'Task Details',
+			component: TaskDetailsPage,
+			passProps: {task: taskItem},
+		});
+	}
+
+	onCreatePressed = () => {
+		this.props.navigator.push({
+			title: 'Create Task',
+			component: CreatePage
+		});
+	}
+
+	checkTaskIsMine = (value) => {
+		return value.isMyTask;
+	}
+
+	onSegmentChanged = (value) => {
+		if (value === 'My Tasks') {
+			this.setState({
+				dataSource: this.ds.cloneWithRows(taskList.filter(this.checkTaskIsMine)),
+				taskView: value,
+			});
+		} else {
+			this.setState({
+				dataSource: this.ds.cloneWithRows(taskList),
+				taskView: value,
+			});
+		}
+	}
+
+	renderIcon = (data) => {
+		if (data.isMyTask) {
+			return (
+				<TouchableOpacity onPress={() => this.onCheckboxPressed(data)}>
+					<Image source={data.completed ? require('../assets/checked.png') : require('../assets/unchecked.png')} 
+						style={styles.checkBoxIcon} />
+				</TouchableOpacity>
+			);
+		} else {
+			return (
+				<Image source={{ uri: data.owner.picURL }} style={styles.photo} />
+			);
+		}
+
+	}
+
+	renderRow = (data) => {
+		var numDays = moment(data.due).fromNow();
+		return (
+			<TouchableOpacity onPress={() => this.onTaskPressed(data)}>
+			  <View style={styles.row}>
+			  	{this.renderIcon(data)}
+			    <Text style={styles.taskName}>{data.name}</Text>
+			    <Text style={styles.due}>{numDays}</Text>
+			  </View>
+			</TouchableOpacity>
+		);
+	}
+
+	render() {
+		return (
+			<View style={{flex: 1, paddingTop: 62}}>
+				<View style={{margin: 12, backgroundColor: 'white'}}>
+					<SegmentedControlIOS 
+						values={["All Tasks", "My Tasks"]} 
+						selectedIndex={0} 
+						tintColor='#319bce'
+						onValueChange={this.onSegmentChanged}/>
+				</View>
+				<View style={styles.segmentSeparator} />
+				<ListView
+					style={{marginTop: -64, zIndex: -100}}
+				  dataSource={this.state.dataSource}
+				  renderRow={this.renderRow}  
+				  renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} /> }/>
+				<TouchableOpacity style={styles.addButton} onPress={() => this.onCreatePressed() }>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+}
+
+export default TasksPage;
+
