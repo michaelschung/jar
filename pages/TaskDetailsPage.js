6b8ec9a57b901a7ebb1d5972cc957bc2fc60fa4b
Michael Chung <mchung96@stanford.edu>
2016-12-02 04:33:08 -0800

    turned all spacing in TaskDetails.js into 4-space wide tabs

--- pages/TaskDetailsPage.js
+++ pages/TaskDetailsPage.js
@@ -25,139 +25,139 @@ var moment = require('moment');
 const dismissKeyboard = require('dismissKeyboard');
 
 const styles = StyleSheet.create({
-  	container: {
+	container: {
 		flexDirection: 'column',
 		flex: 1,
 		paddingTop: 74,
 		margin: 12,
-  	},
+	},
 
-  	/* Styling for details */
-  	taskName: {
+	/* Styling for details */
+	taskName: {
 		fontSize: 24,
 		fontFamily: 'Avenir',
-  	},
-  	detailsContainer: {
+	},
+	detailsContainer: {
 		minHeight: 50,
 		marginTop: 20,
 		flexDirection: 'row',
 		flex: 1,
-  	},
-  	textDetailsContainer: {
+	},
+	textDetailsContainer: {
 		marginLeft: 20,
 		flexDirection: 'column',
 		flex: 1
-  	},
-  	taskNameTextContainer: {
+	},
+	taskNameTextContainer: {
 		marginTop: 15,
 		alignSelf: 'stretch',
 		marginBottom: 20,
-  	},
-  	taskNameText: {
+	},
+	taskNameText: {
 		fontSize: 28,
 		fontWeight: '500',
 		fontFamily: 'Avenir',
-  	},
-  	label: {
+	},
+	label: {
 		textAlign: 'left',
 		fontSize: 20,
 		fontWeight: '500',
 		fontFamily: 'Avenir',
-  	},
-  	dueAndTimeContainer: {
+	},
+	dueAndTimeContainer: {
 		flexDirection: 'column',
-  	},
-  	dueInTextContainer: {
+	},
+	dueInTextContainer: {
 		justifyContent: 'space-between',
 		alignSelf: 'stretch',
 		flexDirection: 'row',
 		marginBottom: 3,
-  	},
-  	dueInText: {
+	},
+	dueInText: {
 		color: '#319bce',
 		textAlign: 'right',
 		fontSize: 20,
 		fontWeight: '400',
 		fontFamily: 'Avenir',
-  	},
-  	dueInTextUrgent: {
+	},
+	dueInTextUrgent: {
 		color: 'red',
 		textAlign: 'right',
 		fontSize: 20,
 		fontWeight: '400',
 		fontFamily: 'Avenir',
-  	},
-  	timeToCompleteTextContainer: {
+	},
+	timeToCompleteTextContainer: {
 		justifyContent: 'space-between',
 		alignSelf: 'stretch',
 		flexDirection: 'row',
 		marginTop: 3,
-  	},
-  	timeToCompleteText: {
+	},
+	timeToCompleteText: {
 		textAlign: 'right',
 		fontSize: 20,
 		fontWeight: '400',
 		fontFamily: 'Avenir',
-  	},
-  	taskOwnerImageContainer: {
+	},
+	taskOwnerImageContainer: {
 		flexDirection: 'column',
 		flex: 1,
 		alignItems: 'flex-end',
-  	},
-  	ownerImage: {
+	},
+	ownerImage: {
 		marginRight: 20,
 		height: 120,
 		width: 120,
 		borderRadius: 60,
-  	},
-  	transferImage: {
+	},
+	transferImage: {
 		marginRight: 20,
 		height: 100,
 		width: 100,
 		borderRadius: 50,
 		opacity: .5,
-  	},
-  	taskStatusContainer: {
+	},
+	taskStatusContainer: {
 		height: 100,
 		width: 100,
 		marginTop: 5,
 		marginRight: 6,
-  	},
-  	status: {
+	},
+	status: {
 		color: '#f0ad4e',
 		fontSize: 18,
 		fontWeight: '500',
-  	},
+	},
 
 
-  	/* Styling for notes */
-  	notesContainer: {
+	/* Styling for notes */
+	notesContainer: {
 		marginTop: -20,
 		marginLeft: 20,
 		flex: 1,
 		flexDirection: 'column',
 		alignItems: 'stretch',
-  	},
+	},
 
-  	/* Styling for separator */
-  	separator: {
+	/* Styling for separator */
+	separator: {
 		borderBottomWidth: 2,
 		borderBottomColor: '#C7C7CD',
 		marginLeft: 20,
 		marginRight: 20,
-  	},
+	},
 
-  	bottomPortionContainer: {
+	bottomPortionContainer: {
 		minHeight: 200,
-  	},
+	},
 
-  	/* Styling for buttons */
-  	buttonsContainer: {
+	/* Styling for buttons */
+	buttonsContainer: {
 		marginTop: 20,
 		flex: 1,
 		alignItems: 'center',
-  	},
-  	button: {
+	},
+	button: {
 		justifyContent: 'center',
 		backgroundColor: '#319bce',
 		marginBottom: 15,
@@ -165,76 +165,76 @@ const styles = StyleSheet.create({
 		minHeight: 75,
 		minWidth: 250,
 		alignItems: 'center',
-  	},
-  	warnButton: {
+	},
+	warnButton: {
 		backgroundColor: '#f0ad4e',
-  	},
-  	disabledButton: {
+	},
+	disabledButton: {
 		backgroundColor: '#979797',
-  	},
-  	buttonText: {
+	},
+	buttonText: {
 		fontWeight: '500',
 		color: 'white',
 		alignSelf: 'center',
 		fontSize: 24,
 		fontFamily: 'Avenir',
-  	},
+	},
 
-  	/* Styling for carousel */
-  	carousel: {
+	/* Styling for carousel */
+	carousel: {
 		flex: 1,
 		flexDirection: 'column',
 		margin: 20,
-  	},
-  	carouselHeader: {
+	},
+	carouselHeader: {
 		flexDirection: 'row',
 		alignItems: 'stretch',
 		justifyContent: 'space-between',
-  	},
-  	carouselHeaderText: {
+	},
+	carouselHeaderText: {
 		textAlign: 'left',
 		fontSize: 16,
 		fontWeight: '600',
 		fontFamily: 'Avenir',
-  	},
-  	carouselCancel: {
+	},
+	carouselCancel: {
 		justifyContent: 'center',
-  	},
-  	carouselCancelButton: {
+	},
+	carouselCancelButton: {
 		height: 17,
 		width: 17,
-  	},
-  	cancelText: {
+	},
+	cancelText: {
 		textAlign: 'center',
 		fontSize: 20,
 		fontWeight: 'bold',
 		fontFamily: 'Avenir',
-  	},
-  	carouselScrollView: {
+	},
+	carouselScrollView: {
 		height: 50,
 		margin: 10, 
-  	},
-  	userContainer: {
+	},
+	userContainer: {
 		flexDirection: 'column',
 		margin: 10,
 		alignItems: 'center',
-  	},
-  	userImage: {
+	},
+	userImage: {
 		height: 60,
 		width: 60,
 		borderRadius: 30,
 		marginBottom: 5,
-  	},
-  	userName: {
+	},
+	userName: {
 		fontWeight: '500',
 		fontFamily: 'Avenir',
-  	},
-  	notes: {
+	},
+	notes: {
 		height: 120,
 		color: '#8F8E94',
 		fontSize: 16,
 		fontFamily: 'Avenir',
-  	},
+	},
 });
 
 
@@ -242,43 +242,43 @@ const styles = StyleSheet.create({
 
 /* Transfer users carousel component */
 class TransferToCarousel extends Component {
- 	constructor(props) {
+	constructor(props) {
 		super(props);
 		/* Construct list of possible users to transfer task to */
 		this.transferToList = props.house.filter((user)=>!user.isMe);
-  	}
+	}
 
-  	/* Render modal stating transfer request was sent */
-  	_onPressTransferIcon = (userData) => {
+	/* Render modal stating transfer request was sent */
+	_onPressTransferIcon = (userData) => {
 		/* Show modal (done in TaskDetailsPage component)*/
 		this.props.showModal(userData)
-  	}
+	}
 
-  	renderUser = (userData, index) => {
+	renderUser = (userData, index) => {
 		return (
-		  	<View key={index}>
+			<View key={index}>
 				<TouchableOpacity onPress={() => this._onPressTransferIcon(userData)}>
-				  	<View style={styles.userContainer}>
+					<View style={styles.userContainer}>
 						<Image source={{ uri: userData.picURL }} style={styles.userImage} />
 						<Text style={styles.userName}>{userData.firstName}</Text>
-				  	</View>
+					</View>
 				</TouchableOpacity>
-		  	</View>
+			</View>
 		);
-  	};
+	};
 
-  	render() {
+	render() {
 		return (
-	  		<ScrollView 
+			<ScrollView 
 				automaticallyAdjustContentInsets={false}
 				alwaysBounceHorizontal={false} 
 				horizontal={true}
 				centerContent = {true}
 				style={styles.carouselScrollView} >
 				{this.transferToList.map(this.renderUser)}
-	  		</ScrollView>
+			</ScrollView>
 		);
-  	}
+	}
 }
 
 
@@ -286,156 +286,156 @@ class TaskDetailsPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-		  	renderPlaceholderOnly: true,
-		  	selectingTransfer: false,
-		  	showModal: false,
-		  	showMessage: false,
-		  	showConfirmTransferModal: false,
-		  	showConfirmCompleteModal: false,
-		  	showConfirmCancelTransferModal: false,
-		  	showConfirmCompleteModal: false,
-		  	showTransferSentMessage: false,
-		  	showTaskCompletedMessage: false,
-		  	transferSent: this.props.task.isAwaitingTransfer,
-		  	disableComplete: this.props.task.isAwaitingTransfer,
-		  	taskCompleted: this.props.task.completed,
-		  	notes: '',
+			renderPlaceholderOnly: true,
+			selectingTransfer: false,
+			showModal: false,
+			showMessage: false,
+			showConfirmTransferModal: false,
+			showConfirmCompleteModal: false,
+			showConfirmCancelTransferModal: false,
+			showConfirmCompleteModal: false,
+			showTransferSentMessage: false,
+			showTaskCompletedMessage: false,
+			transferSent: this.props.task.isAwaitingTransfer,
+			disableComplete: this.props.task.isAwaitingTransfer,
+			taskCompleted: this.props.task.completed,
+			notes: '',
 		};
-  	}
+	}
 
-  	componentWillUpdate() {
+	componentWillUpdate() {
 		LayoutAnimation.easeInEaseOut();
-  	}
+	}
 
-  	/* For rendering transfer carousel when transfer button pressed */
-  	_onPressTransfer = () => {
-		this.setState((state) => ({		  
-		 	selectingTransfer: true,
+	/* For rendering transfer carousel when transfer button pressed */
+	_onPressTransfer = () => {
+		this.setState((state) => ({       
+			selectingTransfer: true,
 		}));
-  	}
+	}
 
-  	/* For removing transfer carousel and rendering buttons when cancel is pressed */
-  	_onPressCancelTransfer = () => {
-		this.setState((state) => ({
-		  	selectingTransfer: false,
-		}));
-  	}
-
-  	/* For showing 'confirm transfer request' modal when transfer icon is pressed */
-  	_onPressTransferIcon = (userData) => {
-		this.setState((state) => ({
-		  	showModal: true,
-		  	showMessage: false,
-		  	showConfirmTransferModal: true,
-		  	transferSentTo: userData,
-		}));
-  	}
-
-  	/* transfer request confirmed */
-  	_onConfirmTransferModal = () => {
+	/* For removing transfer carousel and rendering buttons when cancel is pressed */
+	_onPressCancelTransfer = () => {
 		this.setState((state) => ({
 			selectingTransfer: false,
-	  		showModal: false,
-	  		showMessage: true,
-	  		showConfirmTransferModal: false,
-	  		showTaskTransferedMessage: true,
-	  		transferSentTo: this.state.transferSentTo,
-	  		transferSent: true,
-	  		disableComplete: true,
+		}));
+	}
+
+	/* For showing 'confirm transfer request' modal when transfer icon is pressed */
+	_onPressTransferIcon = (userData) => {
+		this.setState((state) => ({
+			showModal: true,
+			showMessage: false,
+			showConfirmTransferModal: true,
+			transferSentTo: userData,
+		}));
+	}
+
+	/* transfer request confirmed */
+	_onConfirmTransferModal = () => {
+		this.setState((state) => ({
+			selectingTransfer: false,
+			showModal: false,
+			showMessage: true,
+			showConfirmTransferModal: false,
+			showTaskTransferedMessage: true,
+			transferSentTo: this.state.transferSentTo,
+			transferSent: true,
+			disableComplete: true,
 		}));
 		this.props.requestTransfer(this.props.task, this.state.transferSentTo);
-  	}
+	}
 
-  	/* For canceling transfer */
-  	_onPressCancelTransferRequest = () => {
+	/* For canceling transfer */
+	_onPressCancelTransferRequest = () => {
 		this.setState((state) => ({
-		  	showModal: true,
-		  	showConfirmCancelTransferModal: true,
+			showModal: true,
+			showConfirmCancelTransferModal: true,
 		}));
-  	}
+	}
 
-  	/* For confirming to cancel transfer request */
-  	_onConfirmCancelTransferRequest = () => {
+	/* For confirming to cancel transfer request */
+	_onConfirmCancelTransferRequest = () => {
 		this.setState((state) => ({
-		  	showModal: false,
-		  	showMessage: true,
+			showModal: false,
+			showMessage: true,
 			 showConfirmCancelTransferModal: false,
-		  	showTransferCanceledMessage: true,
-		  	transferSent: false,
-		  	disableComplete: false,
+			showTransferCanceledMessage: true,
+			transferSent: false,
+			disableComplete: false,
 		}));
 		this.props.cancelTransfer(this.props.task);
-  	}
+	}
 
-  	/* For showing "Task completed" modal */
-  	_onPressCompleteTask = () => {
+	/* For showing "Task completed" modal */
+	_onPressCompleteTask = () => {
 		this.setState((state) => ({
-		  	showModal: true,
-		  	showConfirmCompleteModal: true,
+			showModal: true,
+			showConfirmCompleteModal: true,
 		}));
-  	}
+	}
 
-  	/* For confirming completion of task */
-  	_onConfirmCompleteTask = () => {
+	/* For confirming completion of task */
+	_onConfirmCompleteTask = () => {
 		this.setState((state) => ({
-	  		showModal: false,
-	  		showConfirmCompleteModal: false,
-	  		disableComplete: true,
-	  		taskCompleted: true,
+			showModal: false,
+			showConfirmCompleteModal: false,
+			disableComplete: true,
+			taskCompleted: true,
 		}));
 
 		/*
 		  Set task as completed for other pages
 		*/
 		this.props.taskCompleted(this.props.task);
-  	}
+	}
 
-  	/* For removing modal */
-  	_onDismissModal = () => {
+	/* For removing modal */
+	_onDismissModal = () => {
 		this.setState((state) => ({
-		  	showModal: false,
-		  	showConfirmTransferModal: false,
-		  	showConfirmCompleteModal: false,
-		  	showConfirmCancelTransferModal: false,
-		  	showConfirmCompleteModal: false,
+			showModal: false,
+			showConfirmTransferModal: false,
+			showConfirmCompleteModal: false,
+			showConfirmCancelTransferModal: false,
+			showConfirmCompleteModal: false,
 		}));
-  	}
+	}
 
-  	setNotes = (text) => {
+	setNotes = (text) => {
 		// console.log("NOTES:", text);
 		this.props.task.notes = text;
-  	}
+	}
 
-  	getPlaceholderNotes() {
+	getPlaceholderNotes() {
 		return this.props.task.notes == '' ? 'Add a note...' : this.props.task.notes;
-  	}
+	}
 
-  	getDefaultNotes() {
+	getDefaultNotes() {
 		return this.props.task.notes == '' ? '' : this.props.task.notes;
-  	}
+	}
 
-  	/* Transfer button */
-  	TransferButton = () => {
+	/* Transfer button */
+	TransferButton = () => {
 		if (this.state.taskCompleted) {
-	  		return (
+			return (
 				<View style={[styles.button, styles.disabledButton]} >
-				  	<Text style={styles.buttonText}>Transfer Task</Text>
+					<Text style={styles.buttonText}>Transfer Task</Text>
 				</View>
-	  		)
+			)
 		}
 		return this.state.transferSent ? (
 			<TouchableOpacity style={[styles.button, styles.warnButton]} onPress={this._onPressCancelTransferRequest} >
-			  	<Text style={styles.buttonText}>Cancel Transfer</Text>
+				<Text style={styles.buttonText}>Cancel Transfer</Text>
 			</TouchableOpacity>
-	  	) : (
+		) : (
 			<TouchableOpacity style={styles.button} onPress={this._onPressTransfer} >
-			  	<Text style={styles.buttonText}>Transfer Task</Text>
+				<Text style={styles.buttonText}>Transfer Task</Text>
 			</TouchableOpacity>
-	  	)
-  	}
+		)
+	}
 
-  	/* Complete button */
-  	CompleteButton = () => {
+	/* Complete button */
+	CompleteButton = () => {
 		return this.state.disableComplete ? (
 			this.state.taskCompleted ? (
 				<View style={[styles.button, styles.disabledButton]} >
@@ -451,14 +451,14 @@ class TaskDetailsPage extends Component {
 				<Text style={styles.buttonText}>Complete Task</Text>
 			</TouchableOpacity>
 		)
-  	}
+	}
 
-  	/* Action buttons */
-  	ActionButtons = () => {
-  		return (
+	/* Action buttons */
+	ActionButtons = () => {
+		return (
 			<View style={styles.buttonsContainer}>
-			  	{this.TransferButton()}
-			  	{this.CompleteButton()}
+				{this.TransferButton()}
+				{this.CompleteButton()}
 			</View>
 		)
 	}
@@ -474,17 +474,17 @@ class TaskDetailsPage extends Component {
 		}
 	}
 
-  	/* Carousel */
-  	Carousel = () => {
-  		return (
+	/* Carousel */
+	Carousel = () => {
+		return (
 			<View style={styles.carousel}>
-			  	<View style={styles.carouselHeader}>
+				<View style={styles.carouselHeader}>
 					<Text style={styles.carouselHeaderText}>Transfer to...</Text>
 					<TouchableOpacity style={styles.carouselCancel} onPress={this._onPressCancelTransfer}>
-					  	<Image style={styles.carouselCancelButton} source={require('../assets/x.png')}/>
+						<Image style={styles.carouselCancelButton} source={require('../assets/x.png')}/>
 					</TouchableOpacity>
-			  	</View>
-			  	<TransferToCarousel showModal={this._onPressTransferIcon} house={this.props.house}></TransferToCarousel>
+				</View>
+				<TransferToCarousel showModal={this._onPressTransferIcon} house={this.props.house}></TransferToCarousel>
 			</View>
 		)
 	}
@@ -527,73 +527,73 @@ class TaskDetailsPage extends Component {
 			cancelCallback={cancelCallback}
 			></ConfirmationModal>);
 		} else {
-		  	modal = null;
+			modal = null;
 		}
 
 		return (
 			<TouchableWithoutFeedback onPress={() => dismissKeyboard()}>
 				<View style={styles.container}>
-				  	{/* Details about task */}
+					{/* Details about task */}
 					<View style={styles.detailsContainer}>
 						<View style={styles.textDetailsContainer}>
-						  	<View style={styles.taskNameTextContainer}>
+							<View style={styles.taskNameTextContainer}>
 								<Text style={styles.taskNameText}>{this.props.task.name}</Text>
-						  	</View>
-						  	<View style={styles.dueAndTimeContainer}>
+							</View>
+							<View style={styles.dueAndTimeContainer}>
 								<View style={styles.dueInTextContainer}>
-							  		<Text style={styles.label}>Due: </Text>
-							  		{/* Use urgent style if task is due within 24 hours (1 day) */}
-							  		<Text style={daysLeft > 1 ? styles.dueInText : styles.dueInTextUrgent}>{numDays}</Text>
+									<Text style={styles.label}>Due: </Text>
+									{/* Use urgent style if task is due within 24 hours (1 day) */}
+									<Text style={daysLeft > 1 ? styles.dueInText : styles.dueInTextUrgent}>{numDays}</Text>
 								</View>
 								<View style={styles.timeToCompleteTextContainer}>
-							  		<Text style={styles.label}>Will Take: </Text>
-							  		<Text style={styles.timeToCompleteText}>{this.props.task.timeToComplete}</Text>
+									<Text style={styles.label}>Will Take: </Text>
+									<Text style={styles.timeToCompleteText}>{this.props.task.timeToComplete}</Text>
 								</View>
-						  	</View>
+							</View>
 						</View>
 						<View style={styles.taskOwnerImageContainer}>
-						  	<Image 
+							<Image 
 								source={{ uri: this.props.task.isAwaitingTransfer ? this.props.task.isAwaitingTransfer.picURL : this.props.task.owner.picURL }} 
 								style={this.props.task.isAwaitingTransfer ? styles.transferImage : styles.ownerImage}
 							/>
-						  	<View style={styles.taskStatusContainer}> 
+							<View style={styles.taskStatusContainer}> 
 								{
 									this.props.task.isAwaitingTransfer ?
 									(<Text style={styles.status}>Awaiting Transfer</Text>) :
 									null
 								}
-						  	</View>
+							</View>
 						</View>
 					</View>
 
-				  	{/* Notes */}
-				  	<View style={styles.notesContainer}>
+					{/* Notes */}
+					<View style={styles.notesContainer}>
 						<Text style={styles.label}>Notes:</Text>
 						<TextInput
-						  	style={styles.notes}
-						  	autoCorrect={true}
-						  	multiline={true}
-						  	numberOfLines={6}
-						  	defaultValue={this.getDefaultNotes()}
-						  	onChangeText={this.setNotes}
-						  	placeholder={this.getPlaceholderNotes()}
-						  	value={this.state.text}
+							style={styles.notes}
+							autoCorrect={true}
+							multiline={true}
+							numberOfLines={6}
+							defaultValue={this.getDefaultNotes()}
+							onChangeText={this.setNotes}
+							placeholder={this.getPlaceholderNotes()}
+							value={this.state.text}
 						/>
-				  	</View>
+					</View>
 
-				  	<View style={styles.separator}></View>
+					<View style={styles.separator}></View>
 
-				  	{/* Action buttons, carousel, or nothing (depending on state of page and task owner) */}
+					{/* Action buttons, carousel, or nothing (depending on state of page and task owner) */}
 					<View style={styles.bottomPortionContainer}>
 						{
 							this.BottomPortion()
 						}
 					</View>
 
-				  	{/* Show modal if transfer request was sent */}
-				  	{
+					{/* Show modal if transfer request was sent */}
+					{
 						modal
-				  	}
+					}
 
 				</View>
 			</TouchableWithoutFeedback>
